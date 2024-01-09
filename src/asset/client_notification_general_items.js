60da117ba5d319623b9f2257616bbfbe7385486d
miku0129 <svartkatt39@gmail.com>
2024-01-09 18:23:00 +0100

    update client_notification_general

--- src/asset/client_notification_general_items.js
+++ src/asset/client_notification_general_items.js
@@ -4,7 +4,7 @@ export const client_notification_gn = {
     "Contactez-nous au travers de la rubrique contact. Indiquez l'article que vous souhaitez acheter. Une vidéo de l'article peut vous être partagée sur demande.",
   text2:
     "Nous convenons ensemble d'un lieu de livraison - Saint François, Le Moule, Sainte Anne, Gosier, Pointe à Pitre ou Jarry. La livraison est en personne. Le contact se fera par WhatsApp.",
-  text3: "Vous réglez un acompte d'une valeur de 50%.",
+  text3: "Pour réserver l'article et obtenir la livraison, vous devrez régler un acompte d'une valeur de 50% via un lien Sumup.",
   text4:
     "Vous réglez le solde à la livraison, par carte bancaire ou en espèces.",
 };
