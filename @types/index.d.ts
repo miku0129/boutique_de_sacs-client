50cae879fbb4401b04ef258fa70296c0c87bbdc5
miku0129 <svartkatt39@gmail.com>
2024-01-15 09:11:46 +0100

    update items

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -17,7 +17,7 @@ type Item = {
   desc_1?: string;
   desc_2?: string;
   category: Category;
-  price: number;
+  price: number | string;
   is_available: boolean;
   item_img_urls?: Item_img_url[];
 };
--- src/utilities/data/item.js
+++ src/utilities/data/item.js
@@ -34,5 +34,495 @@ export const item = {
         },
       ],
     },
+    {
+      id: 2,
+      item_id_number: "202302",
+      name: "Banane bambou",
+      desc_1:
+        "Banane pour femme. Se porte devant la poitrine ou autour de la taille. Peut contenir un smartphone, une paire de lunettes et un porte-monnaie.",
+      desc_2:
+        "Tissus : toile à sac imperméable et jacquard. Nécessite d'être imperméabilisé régulièrement. Modèle sous licence Dodynette.",
+      category: "sacs",
+      price: 50,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/wJzkbzC/20240107-142002.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/fSXbtgJ/20231009-125531.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/Tb7PhfR/20240107-141924.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 3,
+      item_id_number: "202303",
+      name: "Banane gingko jaune",
+      desc_1:
+        "Banane pour femme. Se porte devant la poitrine ou autour de la taille. Peut contenir un smartphone, une paire de lunettes et un porte-monnaie.",
+      desc_2:
+        "Tissus : toile à sac imperméable et coton. Nécessite d'être imperméabilisé régulièrement. Modèle sous licence Dodynette.",
+      category: "sacs",
+      price: 50,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/72xZ6C0/20240107-141845.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/72xZ6C0/20240107-141845.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      item_id_number: "202304",
+      name: "Banane éventails pourpre",
+      desc_1:
+        "Banane pour femme. Se porte devant la poitrine ou autour de la taille. Peut contenir un smartphone, une paire de lunettes et un porte- monnaie.",
+      desc_2:
+        "Tissus : toile à sac imperméable et coton enduit. Modèle sous licence Dodynette.",
+      category: "sacs",
+      price: 55,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/mFv0yQv/20240107-141815.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/jkfzTrz/20240107-141810.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/phqv3xt/20231009-125708.jpg",
+          is_main: false,
+        },
+        {
+          id: 4,
+          url: "https://i.ibb.co/pz30Nkw/20240107-141743.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 5,
+      item_id_number: "202305",
+      name: "Banane wax bleu et violet",
+      desc_1:
+        "Banane pour femme. Se porte devant la poitrine ou autour de la taille. Peut contenir un smartphone, une paire de lunettes et un porte-monnaie.",
+      desc_2:
+        "Tissus : simili cuir et coton. Nécessite d'être imperméabilisé régulièrement. Modèle sous licence Dodynette.",
+      category: "sacs",
+      price: 50,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/FDd6t3C/20240107-141521.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/gRWYn8g/20240107-141958.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/vPcs3dL/20231009-125804.jpg",
+          is_main: false,
+        },
+        {
+          id: 4,
+          url: "https://i.ibb.co/McDCK0L/20240107-141504.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 6,
+      item_id_number: "202306",
+      name: "Banane Afrika",
+      desc_1:
+        "Banane pour femme. Se porte devant la poitrine ou autour de la taille. Peut contenir un smartphone, une paire de lunettes et un porte-monnaie.",
+      desc_2:
+        "Tissus : simili cuir et coton. Nécessite d'être imperméabilisé régulièrement. Modèle sous licence Dodynette.",
+      category: "sacs",
+      price: 50,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/BGpdxXg/20240107-141440.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/K2GSPrX/20240107-141432.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/LnWDmLJ/20231009-125850.jpg",
+          is_main: false,
+        },
+        {
+          id: 4,
+          url: "https://i.ibb.co/8rHMHmv/20240107-141418.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 7,
+      item_id_number: "202307",
+      name: "Banane wax rose",
+      desc_1:
+        "Banane pour femme. Se porte devant la poitrine ou autour de la taille. Peut contenir un smartphone, une paire de lunettes et un porte-monnaie.",
+      desc_2:
+        "Tissus : simili cuir et coton. Nécessite d'être imperméabilisé régulièrement. Modèle sous licence Dodynette.",
+      category: "sacs",
+      price: 50,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/SrKwppr/20240107-141304.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/gRWYn8g/20240107-141958.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/V9Hd9XY/20240107-141246.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 8,
+      item_id_number: "202308",
+      name: "Sac à dos bubble",
+      desc_1:
+        "Petit sac à dos pour enfant ou femme. Peut contenir une gourde et une paire de chaussures ou un vêtement ainsi qu'un porte-feuille.",
+      desc_2:
+        "Tissu : simili cuir et coton. Nécessite d'être imperméabilisé régulièrement. Modèle sous licence Dodynette.",
+      category: "sacs",
+      price: 50,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/4mbFM7f/20240107-151138.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/mq3WFHb/20240107-151157.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/ggjLCJb/20240107-151130.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 9,
+      item_id_number: "202309",
+      name: "Sac bandoulière bleu roi",
+      desc_1:
+        "Grand sac bandoulière. Peut contenir un agenda, un PC portable 14' et son chargeur, une pochette A4, une trousse, un smartphone et un portefeuille.",
+      desc_2:
+        "Tissus : jacquard, coton et suédine. Nécessite d'être imperméabilisé régulièrement. Modèle sous licence Sacôtin.",
+      category: "sacs",
+      price: 80,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/RDM4xrC/20240107-140557.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/RDM4xrC/20240107-140557.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/N6XxYD8/20240107-140932.jpg",
+          is_main: false,
+        },
+        {
+          id: 4,
+          url: "https://i.ibb.co/rvFkN4T/20240107-140922.jpg",
+          is_main: false,
+        },
+
+        {
+          id: 5,
+          url: "https://i.ibb.co/sb13zfm/20240107-140538.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 10,
+      item_id_number: "202310",
+      name: "Sac bandoulière bambou",
+      desc_1:
+        "Sac bandoulière moyen. Peut contenir un portefeuille, une tablette, un smartphone et un paquet de mouchoirs.",
+      desc_2:
+        "Tissus : jacquard, coton et suédine. Nécessite d'être imperméabilisé régulièrement. Modèle sous licence Sacôtin.",
+      category: "sacs",
+      price: 80,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/k1XtKDf/20240107-140725.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/df0z99Z/20240107-141117.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/C9yFCtr/20240107-141112.jpg",
+          is_main: false,
+        },
+        {
+          id: 4,
+          url: "https://i.ibb.co/rbXJ7Hg/20240107-140659.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 11,
+      item_id_number: "202311",
+      name: "Panier cabas",
+      desc_1: "Panier tressé en forme de cabas. Peut supporter 3 kilos.",
+      desc_2: "Matière : rotin.",
+      category: "vannerie",
+      price: 70,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/nBSgsTx/20240107-153851.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/NppbTXr/20240107-153905.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 12,
+      item_id_number: "202312",
+      name: "Panier rond profond",
+      desc_1: "Panier tressé en forme de boule. Peut supporter 6 kilos.",
+      desc_2: "Matière : rotin.",
+      category: "vannerie",
+      price: 80,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/zbTdFLr/20240107-153924.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/fMG4bys/20240107-153929.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 13,
+      item_id_number: "202313",
+      name: "Panier ovale",
+      desc_1: "Grand panier tressé en forme ovale. Peut contenir 6 kilos.",
+      desc_2: "Matière : rotin.",
+      category: "vannerie",
+      price: 85,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/HdvyRMB/20240107-153950.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/9WQM1tN/20240107-153954.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/6tcgzNw/20240107-153943.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 14,
+      item_id_number: "202314",
+      name: "Panière bleu",
+      desc_1:
+        "Grande panière ronde avec anses sur les côtés. Peut être utilisée comme panier à linge.",
+      desc_2: "Matière : rotin tinté bleu.",
+      category: "vannerie",
+      price: 75,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/vxGY5NK/20230913-135943.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/pPtMB0Z/20230913-140035.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/kmRp9LN/20230913-140019.jpg",
+          is_main: false,
+        },
+        {
+          id: 4,
+          url: "https://i.ibb.co/zPJMgQY/20230913-135948.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 15,
+      item_id_number: "202315",
+      name: "Corbeille à fruits foncée",
+      desc_1: "Corbeille à fruits pour salle à manger.",
+      desc_2: "Matière : rotin",
+      category: "vannerie",
+      price: 30,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/23JM8vq/20240107-153826.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/1zRzRLG/20240107-153833.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 16,
+      item_id_number: "202316",
+      name: "Corbeille à fruits claire",
+      desc_1: "Corbeille à fruits pour salle à manger.",
+      desc_2: "Matière : rotin",
+      category: "vannerie",
+      price: 30,
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/HppYZ3M/20230913-140134.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/3zRVQb1/20230913-140123.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/wg0Mc1h/20230913-140102.jpg",
+          is_main: false,
+        },
+        {
+          id: 4,
+          url: "https://i.ibb.co/tM6R8rG/20230913-140053.jpg",
+          is_main: true,
+        },
+      ],
+    },
+    {
+      id: 17,
+      item_id_number: "202317",
+      name: "Chaise de salon de jardin",
+      desc_1:
+        "Chaise lavée, poncée, dégrisée puis huilée. Après le perçage des trous tout autour de l'assise et du dossier, le tressage est fait selon les techniques de cannage.",
+      desc_2:
+        "Prix uniquement sur devis et après avoir touché l'objet à rénover.",
+      category: "autre",
+      price: "Commandes reçues",
+      is_avairable: true,
+      item_img_urls: [
+        {
+          id: 1,
+          url: "https://i.ibb.co/0n7Mj7n/20211129-172835.jpg",
+          is_main: false,
+        },
+        {
+          id: 2,
+          url: "https://i.ibb.co/th17fd2/20220715-164933.jpg",
+          is_main: false,
+        },
+        {
+          id: 3,
+          url: "https://i.ibb.co/yQv0hY4/20221118-165641.jpg",
+          is_main: false,
+        },
+        {
+          id: 5,
+          url: "https://i.ibb.co/nC3pQdR/20220910-085505.jpgg",
+          is_main: false,
+        },
+        {
+          id: 6,
+          url: "https://i.ibb.co/yBxrGwZ/20220424-120108.jpg",
+          is_main: true,
+        },
+
+        {
+          id: 4,
+          url: "https://i.ibb.co/tM6R8rG/20230913-140053.jpg",
+          is_main: true,
+        },
+      ],
+    },
   ],
 };
