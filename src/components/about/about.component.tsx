eeca69357aae977ba3baf4124ba592edd065585c
miku0129 <svartkatt39@gmail.com>
2024-01-16 12:11:07 +0100

    update README and About

--- src/components/about/about.component.tsx
+++ src/components/about/about.component.tsx
@@ -20,6 +20,7 @@ const About = () => {
             src="https://images.unsplash.com/photo-1501644898242-cfea317d7faf?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="bio"
           />
+          <p>&copy;Drew Dizzy Graham</p>
         </div>
         <div className="about-text-container">
           <p>{about_text_1}</p>
