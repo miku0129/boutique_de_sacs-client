dd3c39a1f4045095a821f977b041fc8f29cc3709
miku0129 <svartkatt39@gmail.com>
2024-01-16 12:01:19 +0100

    add text to About

--- src/asset/asset.js
+++ src/asset/asset.js
@@ -16,3 +16,11 @@ export const header_category_3 = "Rénovation de chaises";
 export const item_purchase_button_text = "Contactez le vendeur"
 
 export const price_or_notification_text = "Article sur commande"
+
+export const about_text_1 = "Mon parcours créatif a commencé en 2016 avec ma première machine, offerte par ma famille! Ma mère m'a transmis les bases de la couture, puis, j'ai approfondi mes compétences grâce aux multiples tutoriels en ligne!"
+
+export const about_text_2 = "En 2021, j'ai gagné une formation vannerie avec Mariko'w, situé à Saint François, que j'ai complétée par une formation cannage! Grâce aux bases que m'a enseignées Mariko'w ainsi qu'à ma motivation personnelle j'ai continué à tresser des paniers et je me suis lancée dans la rénovation de mobilier!"
+
+export const about_text_3 = "Passionnée, je me laisse guider par l'inspiration et les fibres naturelles!"
+
+export const about_text_4 = "En utilisant des techniques traditionnelles, je découvre les aspects de cette belle matière en la transformant en objets décoratifs et fonctionnels tels que les paniers et corbeilles!"
