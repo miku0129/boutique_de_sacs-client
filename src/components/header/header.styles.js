003022470dfe937127222c14f7ea53f832941c5c
miku0129 <svartkatt39@gmail.com>
2024-01-30 14:20:25 +0100

    style: update style in Header

---
+++ src/components/header/header.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { CustomLink } from "../../utilities/components.styles";
+
+export const CustomHeaderMenuLink = styled(CustomLink)`
+  color: darkgreen;
+`;
