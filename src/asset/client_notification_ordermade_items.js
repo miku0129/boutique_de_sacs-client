891de9baf5c5588c4e6dee6ace457515b838f599
miku0129 <svartkatt39@gmail.com>
2024-02-03 11:36:23 +0100

    docs: update text of client_notification

--- src/asset/client_notification_ordermade_items.js
+++ src/asset/client_notification_ordermade_items.js
@@ -1,7 +1,7 @@
 export const client_notification_od = {
   text0: "Pour commander un article sur-mesures",
   text1:
-    "Contactez-nous au travers de la rubrique 'contact'. Décrivez l'article souhaité en faisant des comparaisons avec des articles de la boutique, en indiquant les couleurs et matières souhaitées, en précisant les dimensions désirées.",
+    "Contactez-nous au travers de la rubrique 'contactez le vendeur'. Décrivez l'article souhaité en faisant des comparaisons avec des articles de la boutique, en indiquant les couleurs et matières souhaitées, en précisant les dimensions désirées.",
   text2:
     "Nous vous ferons des suggestions et proposerons devis.",
   text3:
--- src/asset/client_notification_repair_chair.js
+++ src/asset/client_notification_repair_chair.js
@@ -1,6 +1,6 @@
 export const client_notification_rp = {
   text0: "Pour réparation et restauration",
-  text1: "Contactez-nous au travers de la rubrique 'contact'. ",
+  text1: "Contactez-nous au travers de la rubrique 'contactez le vendeur'.",
   text2: "Prenez un RDV avec nous. Prenez rendez-vous avec nous. Précisez plusieurs disponibilités.",
   text3:
     "Afin de vous proposer un devis juste, il est nécessaire de voir l'état de la ou des chaises. Une fois le devis accepté, acompte de 50% devra être versé.",
