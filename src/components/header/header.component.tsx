8f279a71326f6e489fdea57e323a141334fb7328
miku0129 <svartkatt39@gmail.com>
2024-02-20 15:19:05 +0100

    update Header with new shop-logo

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -13,6 +13,7 @@ import { header_category_3 } from "../../asset/asset";
 
 import { CustomLink } from "../../utilities/components.styles";
 import { CustomHeaderMenuLink } from "./header.styles";
+import { ReactComponent as ShopLogo } from "./../../asset/logo.svg";
 import "./header.styles.scss";
 
 const Header = () => {
@@ -20,27 +21,40 @@ const Header = () => {
     <Fragment>
       <Navbar expand="lg" className="custom-navbar">
         <Container>
-          <CustomLink to={"/"}>
-            <span className="custom-navbar-main-title">{shop_name}</span>
-          </CustomLink>
+          <div className="shoptitle-contaier">
+            <div>
+              <ShopLogo style={{ height: "40px" }} />
+            </div>
+            <CustomLink to={"/"}>
+              <span className="custom-navbar-main-title">{shop_name}</span>
+            </CustomLink>
+          </div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <CustomHeaderMenuLink to={"/about"} className="custom-header-menu-link-about">
+              <CustomHeaderMenuLink
+                to={"/about"}
+                className="custom-header-menu-link-about"
+              >
                 {header_about}
               </CustomHeaderMenuLink>
               <NavDropdown title="boutique" id="basic-nav-dropdown">
                 <CustomHeaderMenuLink to={"/"} state={{ previewtype: "sacs" }}>
                   {header_category_1}
                 </CustomHeaderMenuLink>
-                <CustomHeaderMenuLink to={"/"} state={{ previewtype: "vannerie" }}>
+                <CustomHeaderMenuLink
+                  to={"/"}
+                  state={{ previewtype: "vannerie" }}
+                >
                   {header_category_2}
                 </CustomHeaderMenuLink>
                 <CustomHeaderMenuLink to={"/"} state={{ previewtype: "autre" }}>
                   {header_category_3}
                 </CustomHeaderMenuLink>
                 <NavDropdown.Divider />
-                <CustomHeaderMenuLink to={"/contact"}>Contact</CustomHeaderMenuLink>
+                <CustomHeaderMenuLink to={"/contact"}>
+                  Contact
+                </CustomHeaderMenuLink>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
