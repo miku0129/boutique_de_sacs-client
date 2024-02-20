99c041c43d367f0bf3c4ce5008fee8c0ef3f53e2
miku0129 <svartkatt39@gmail.com>
2024-02-20 14:58:14 +0100

    style: add shop logo

--- src/asset/asset.js
+++ src/asset/asset.js
@@ -1,4 +1,4 @@
-export const shop_name = "CréAz";
+export const shop_name = "Kréya-D-zil";
 
 export const shop_concept_1 =
   "Créatrice vous propose des pièces uniques et originales, réalisées à Sainte Anne!";
@@ -13,17 +13,21 @@ export const header_category_1 = "Sacs";
 export const header_category_2 = "Vannerie";
 export const header_category_3 = "Rénovation de chaises";
 
-export const item_purchase_button_text = "Contactez le vendeur"
+export const item_purchase_button_text = "Contactez le vendeur";
 
-export const price_or_notification_text = "Article sur commande"
+export const price_or_notification_text = "Article sur commande";
 
-export const about_text_1 = "Mon parcours créatif a commencé en 2016 avec ma première machine, offerte par ma famille! Ma mère m'a transmis les bases de la couture, puis, j'ai approfondi mes compétences grâce aux multiples tutoriels en ligne!"
+export const about_text_1 =
+  "Mon parcours créatif a commencé en 2016 avec ma première machine, offerte par ma famille! Ma mère m'a transmis les bases de la couture, puis, j'ai approfondi mes compétences grâce aux multiples tutoriels en ligne!";
 
-export const about_text_2 = "En 2021, j'ai gagné une formation vannerie avec Mariko'w, situé à Saint François, que j'ai complétée par une formation cannage! Grâce aux bases que m'a enseignées Mariko'w ainsi qu'à ma motivation personnelle j'ai continué à tresser des paniers et je me suis lancée dans la rénovation de mobilier!"
+export const about_text_2 =
+  "En 2021, j'ai gagné une formation vannerie avec Mariko'w, situé à Saint François, que j'ai complétée par une formation cannage! Grâce aux bases que m'a enseignées Mariko'w ainsi qu'à ma motivation personnelle j'ai continué à tresser des paniers et je me suis lancée dans la rénovation de mobilier!";
 
-export const about_text_3 = "Passionnée, je me laisse guider par l'inspiration et les fibres naturelles!"
+export const about_text_3 =
+  "Passionnée, je me laisse guider par l'inspiration et les fibres naturelles!";
 
-export const about_text_4 = "En utilisant des techniques traditionnelles, je découvre les aspects de cette belle matière en la transformant en objets décoratifs et fonctionnels tels que les paniers et corbeilles!"
+export const about_text_4 =
+  "En utilisant des techniques traditionnelles, je découvre les aspects de cette belle matière en la transformant en objets décoratifs et fonctionnels tels que les paniers et corbeilles!";
 
-export const about_text_5 = "Atelier à Saint Paul, 97180 Sainte Anne"
-export const about_text_6 = "Siret : 814072328000 10"
+export const about_text_5 = "Atelier à Saint Paul, 97180 Sainte Anne";
+export const about_text_6 = "Siret : 814072328000 10";
