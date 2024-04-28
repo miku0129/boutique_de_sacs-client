32f97d0a58c01f1810a1f518910b9ae62c451998
miku0129 <svartkatt39@gmail.com>
2024-04-28 17:01:40 +0200

    feat: add hooks of Hanko to get user's data

--- src/components/admin-dashboard/admin-dashboard.component.tsx
+++ src/components/admin-dashboard/admin-dashboard.component.tsx
@@ -1,17 +1,36 @@
 import HankoLogoutBtn from "../../utilities/hanko/hanko-logout-btn/hanko-logout-btn.component";
-import { CustomLink, CustomBtn, CustomBtnLow } from "../../utilities/components.styles";
+import { useUserData } from "../../utilities/hanko/hanko-useUserData.hooks";
+import {
+  CustomLink,
+  CustomBtn,
+  CustomBtnLow,
+} from "../../utilities/components.styles";
 import "./admin-dashboard.styles.scss";
 
 const AdminDashboard = () => {
+  const {
+    // id,
+    email,
+    loading: userDataLoading,
+    error: userDataError,
+  } = useUserData();
+
+  if (userDataLoading) {
+    return <div>Loading...</div>;
+  }
+  if (userDataError) {
+    return <div>{userDataError}</div>;
+  }
+
   return (
     <div>
+      <p>Bonjour, {email}</p>
       <CustomBtnLow>
         <CustomLink to={"/admin/profile"}>
           <CustomBtn>Profile</CustomBtn>
         </CustomLink>
         <HankoLogoutBtn />
       </CustomBtnLow>
-      <h1>dashboard</h1>
     </div>
   );
 };
