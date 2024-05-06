994882eb697be4b5b76e4ccc3b8bd04451bfdb00
miku0129 <svartkatt39@gmail.com>
2024-05-06 22:17:14 +0200

    docs: revert console.log

--- src/components/admin-login/admin-login.component.tsx
+++ src/components/admin-login/admin-login.component.tsx
@@ -1,9 +1,7 @@
 import HankoAuth from "../../utilities/hanko/hanko-auth.component";
 
 const AdminLogin = () => {
-  console.log("env", import.meta.env.PROD)
-
-  return <HankoAuth />
+  return <HankoAuth />;
 };
 
-export default AdminLogin
+export default AdminLogin;
